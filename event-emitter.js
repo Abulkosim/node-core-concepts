f142e691cf9ead13c802e406ba2de2e60f0724d0
Abulkosim <abulkasimkattaev@gmail.com>
2025-04-27 12:10:09 +0500

    init: node core concepts

---
+++ event-emitter.js
@@ -0,0 +1,38 @@
+class EventEmitter {
+  constructor() {
+    this.events = {};
+  }
+
+  // Subscribe to an event
+  on(event, listener) {
+    if (!this.events[event]) {
+      this.events[event] = [];
+    }
+    this.events[event].push(listener);
+  }
+
+  // Emit an event
+  emit(event, ...args) {
+    if (this.events[event]) {
+      this.events[event].forEach(listener => listener(...args));
+    }
+  }
+
+  // Unsubscribe a listener
+  off(event, listenerToRemove) {
+    if (!this.events[event]) return;
+
+    this.events[event] = this.events[event].filter(
+      listener => listener !== listenerToRemove
+    );
+  }
+
+  // Subscribe only once
+  once(event, listener) {
+    const wrapper = (...args) => {
+      listener(...args);
+      this.off(event, wrapper);
+    };
+    this.on(event, wrapper);
+  }
+}
