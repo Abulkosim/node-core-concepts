8bdd6145b5dc0475a826ae2f99e1132b883edace
Abulkosim <abulkasimkattaev@gmail.com>
2025-11-01 17:31:23 +0500

    update: streams readable

--- streams.js
+++ streams.js
@@ -1,37 +1,16 @@
-import fs from "fs";
+import { Readable } from 'stream';
+class MyStream extends Readable {
+  #count = 0;
+  _read(size) {
+    this.push(':-)');
+    if (++this.#count === 5) {
+      this.push(null);
+    }
+  }
+}
 
-// const readableStream = fs.createReadStream('input.txt', 'utf8');
-// const writableStream = fs.createWriteStream('output.txt');
+const stream = new MyStream();
 
-// readableStream.pipe(writableStream);
-
-// writableStream.on('finish', () => {
-//   console.log('File copy completed!');
-// });
-
-// readableStream.on('error', (err) => {
-//   console.error('Error reading file:', err);
-// });
-
-// writableStream.on('error', (err) => {
-//   console.error('Error writing file:', err);
-// });
-
-
-const readableStream = fs.createReadStream('input.txt', {
-  encoding: 'utf8',
-  highWaterMark: 1 * 1024 
-});
-
-readableStream.on('data', (chunk) => {
-  console.log(`Received ${chunk.length} bytes of data.`);
-  console.log(chunk);
-});
-
-readableStream.on('end', () => {
-  console.log('No more data to read.');
-});
-
-readableStream.on('error', (err) => {
-  console.error('Error reading from stream:', err);
+stream.on('data', chunk => {
+  console.log(chunk.toString());
 });
