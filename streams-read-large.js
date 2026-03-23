cc66dd42bb24848398825e6bb1c3ee3f414f2418
Abulkosim <abulkasimkattaev@gmail.com>
2026-03-23 16:05:32 +0500

    add large file stream reader with line/word count and longest line

--- streams-read-large.js
+++ streams-read-large.js
@@ -3,6 +3,7 @@ import { createReadStream } from 'node:fs';
 const read = (name) => {
   let lines = 0;
   let words = 0;
+  let longestLine = '';
   let lineRemainder = '';
   let wordRemainder = '';
 
@@ -16,20 +17,29 @@ const read = (name) => {
     lineRemainder = lineParts.pop();
     lines += lineParts.length;
 
+    for (const line of lineParts) {
+      if (line.length > longestLine.length) longestLine = line;
+    }
+
     const wordParts = (wordRemainder + chunk).split(/\s+/);
     wordRemainder = wordParts.pop();
     words += wordParts.filter(Boolean).length;
-
 
     const mem = process.memoryUsage();
     console.log(`heapUsed: ${(mem.heapUsed / 1024 / 1024).toFixed(2)} MB`);
   });
 
   stream.on('end', () => {
-    if (lineRemainder.length > 0) lines++;
+    if (lineRemainder.length > 0) {
+      lines++;
+      if (lineRemainder.length > longestLine.length) longestLine = lineRemainder;
+    }
     if (wordRemainder.trim().length > 0) words++;
+
     console.log('lines:', lines);
     console.log('words:', words);
+    console.log('longest line length:', longestLine.length);
+    console.log('longest line:', longestLine);
   });
 
   stream.on('error', (err) => {
